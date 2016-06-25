186d0965fc935a09fa0bd52920bc562f8f178385
Satyam <satyam.gupta2@gmail.com>
2016-06-25 17:58:52 +0530

    Auto/Manual creation of pickrr shipments and updated composer.json and README.md

--- Helper/ExportShipment.php
+++ Helper/ExportShipment.php
@@ -5,7 +5,7 @@ namespace Pickrr\Magento2\Helper;
 use \Magento\Framework\App\Config\ScopeConfigInterface;
 use Magento\Framework\Exception\LocalizedException;
 use Magento\Sales\Model\Order\Shipment;
-
+use Magento\Sales\Model\Order;
 
 class ExportShipment
 extends \Magento\Framework\App\Helper\AbstractHelper
@@ -17,6 +17,164 @@ extends \Magento\Framework\App\Helper\AbstractHelper
         $this->_objectManager = $objectManager;
         $this->scopeConfig = $scopeConfig;
     }
+
+    // Some code referred from: http://www.anyknowledge.com/magento-programmatically-add-shipment-with-its-tracking-number-to-any-specific-order/
+
+    public function completeShipment($order, $shipmentTrackingNumber)
+    {
+        // $shipmentTrackingNumber = 'P01234';
+     
+        $customerEmailComments = '';
+     
+        if (!$order->getId()) {
+            throw new LocalizedException(__("Order does not exist, for the Shipment process to complete"));
+        }
+     
+        if ($order->canShip()) {
+            try {
+                $shipment = $this->_objectManager->get('Magento\Sales\Model\Order\ShipmentFactory');
+
+                $shipmentCarrierCode = 'custom';
+                $shipmentCarrierTitle = 'Pickrr';
+     
+                $arrTracking = array(
+                    'carrier_code' => isset($shipmentCarrierCode) ? $shipmentCarrierCode : $order->getShippingCarrier()->getCarrierCode(),
+                    'title' => isset($shipmentCarrierTitle) ? $shipmentCarrierTitle : $order->getShippingCarrier()->getConfigData('title'),
+                    'number' => $shipmentTrackingNumber,
+                );
+     
+                $track = $this->_objectManager->get('Magento\Sales\Model\Order\Shipment\Track')->addData($arrTracking);
+
+                $shipment =  $shipment->create($order, $this->_getItemQtys($order), array($arrTracking));
+                $shipment->register();
+                $shipment->save();
+
+                $emailSentStatus = $shipment->getData('email_sent');
+                if (!is_null($customerEmailComments) && !$emailSentStatus) {
+                    // $this->_objectManager->get('Magento\Sales\Model\Order\Email\Sender\ShipmentSender')->send($shipment);
+                    $shipment->setEmailSent(true);
+                }
+
+                $this->_saveOrder($order);
+
+            } catch (Exception $e) {
+                throw new LocalizedException(__($e->getMessage()));
+            }
+        }
+    }
+     
+    /**
+     * Get the Quantities shipped for the Order, based on an item-level
+     * This method can also be modified, to have the Partial Shipment functionality in place
+     *
+     * @param $order Mage_Sales_Model_Order
+     * @return array
+     */
+    protected function _getItemQtys(Order $order)
+    {
+        $qty = array();
+     
+        foreach ($order->getAllItems() as $_eachItem) {
+            if ($_eachItem->getParentItemId()) {
+                $qty[$_eachItem->getParentItemId()] = $_eachItem->getQtyOrdered();
+            } else {
+                $qty[$_eachItem->getId()] = $_eachItem->getQtyOrdered();
+            }
+        }
+     
+        return $qty;
+    }
+     
+    /**
+     * Saves the Order, to complete the full life-cycle of the Order
+     * Order status will now show as Complete
+     *
+     * @param $order Mage_Sales_Model_Order
+     */
+    protected function _saveOrder(Order $order)
+    {
+        $order->setState(Order::STATE_PROCESSING);
+        $order->setStatus(Order::STATE_PROCESSING);
+     
+        $order->save();
+    }
+
+    public function createShipment($auth_token, $item_name, $pickup_time, $from_name, $from_phone_number, $from_pincode, $from_address, $to_name, $to_phone_number, $to_pincode, $to_address, $order_id = 'NULL', $cod=0.0)
+    {
+        try{
+
+          $from_pincode = $to_pincode = '245101';
+
+          $params = array(
+                      'auth_token' => $auth_token,
+                      'item_name' => $item_name,
+                      'order_time' => $pickup_time,
+                      'from_name' => $from_name,
+                      'from_phone_number' => $from_phone_number,
+                      'from_pincode'=> $from_pincode,
+                      'from_address'=> $from_address,
+                      'to_name'=> $to_name,
+                      'to_phone_number' => $to_phone_number,
+                      'to_pincode' => $to_pincode,
+                      'to_address' => $to_address,
+                      'client_order_id' => $order_id
+                    );
+
+            if($cod>0.0) $params['cod'] = $cod;
+
+            $json_params = json_encode( $params );
+
+            $url = 'http://www.pickrr.com/api/place-order/';
+            //open connection
+            $ch = curl_init();
+
+            //set the url, number of POST vars, POST data
+            curl_setopt($ch,CURLOPT_URL, $url);
+            curl_setopt($ch,CURLOPT_POSTFIELDS, $json_params);
+            curl_setopt( $ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
+            curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
+
+            //execute post
+            $result = curl_exec($ch);
+            $result = json_decode($result, true);
+
+            //close connection
+            curl_close($ch);
+
+            return $result['tracking_id'];
+
+        }
+        catch (\Exception $e) {
+            throw new LocalizedException(__('There was an error in creating the Pickrr shipment: %1.', $e->getMessage()));
+        }
+
+
+    }
+
+    public function createOrderShipment($auth_token, $order, $pickup_time, $from_name, $from_phone_number, $from_pincode, $from_address, $cod=0.0)
+    {
+        try{
+            $itemCount = $order->getTotalItemCount();
+            $item_name = "NULL";
+
+            if($itemCount==1) $item_name = $order->getItemsCollection()->getFirstItem()->getName();
+            else $item_name = 'Multiple Items';
+
+            $shipping_address = $order->getShippingAddress();            
+            $to_name = $shipping_address->getName();
+            $to_phone_number = $shipping_address->getTelephone();
+            $to_pincode = $shipping_address->getPostcode();
+            $to_address = implode(', ', $shipping_address->getStreet()) . ", " . $shipping_address->getCity() . ", " . $shipping_address->getRegion();
+            $order_id = $order->getIncrementId();
+
+            $tracking_no = $this->createShipment($auth_token, $item_name, $pickup_time, $from_name, $from_phone_number, $from_pincode, $from_address, $to_name, $to_phone_number, $to_pincode, $to_address, $order_id, $cod);
+
+            $this->completeShipment($order, $tracking_no);
+        }
+        catch (\Exception $e) {
+            throw new LocalizedException(__('There was an error in creating a Pickrr shipment using order object: %1.', $e->getMessage()));
+        }
+      }
 
     public function export(Shipment $shipment)
     {
@@ -32,7 +190,7 @@ extends \Magento\Framework\App\Helper\AbstractHelper
                   'customer_name' => $customer->getName(),
                   'customer_email' => $customer->getEmail(),
                   'billing_address' => $billing_address->debug(),
-                  'shipping_address' => $shipping_address->debug(),//$shipment->getAddress(),//$shipping_address,$shipment->getShippingAddressId(),//$order->getShippingAddress(),
+                  'shipping_address' => $shipping_address->debug(),
                   'entity_id' => $shipment->getEntityId(),
                   'created_at' => $shipment->getCreatedAt(),
                   'tracks' => array()
--- README.md
+++ README.md
@@ -2,5 +2,66 @@
 
 Pickrr Magento 2 module for automatic/manual importing of shipment/tracking details in your pickrr account.
 
-#### Features currently in progress: 
-* Track status notifications
+###Installation Instructions:
+
+* Extract the zip file in the the <root_of_Magento2>/app/code
+* Goto root folder of magento in terminal, and run: 
+```shell
+bin/magento module:enable Pickrr_Magento2
+bin/magento setup:upgrade
+
+```
+* Verify in Magento Admin Panel whether the module is enabled. To check go to Admin>Stores>Configuration>Advanced>Advanced>Pickrr_magento1
+
+---
+
+###Usage Instructions:
+
+####Import helper class:
+
+```php
+//import helper class
+
+Pickrr\Magento2\Helper\ExportShipment $helper;
+
+```
+
+####Create a simple Pickrr Shipment:
+
+**Prototype of the function:**
+```php
+createShipment($auth_token, $item_name, $pickup_time, $from_name, $from_phone_number, $from_pincode, $from_address, $to_name, $to_phone_number, $to_pincode, $to_address, $order_id = 'NULL', $cod=0.0);
+```
+
+It returns the tracking_id from Pickrr.
+
+**Usage:**
+```php
+//Create shipment using order
+
+$auth_key =  'Your Auth Key';
+
+$helper->createOrderShipment($auth_key, "Item's Name", '2016-06-17 17:00', "Merchant/Sender's Name", "Merchant/Sender's Phone", 'Pickup Address Pin', 'Pickup Address');
+```
+
+---
+
+####Create Shipment using order:
+
+This will also create shipment and associate it with the passed order. The client/customer's address, item's name and order's id will be extracted from order.
+
+**Prototype of the function:**
+```php
+createOrderShipment($auth_token, $order, $pickup_time, $from_name, $from_phone_number, $from_pincode, $from_address, $cod=0.0);
+
+```
+
+**Usage:**
+```php
+//Create shipment using order
+
+$auth_key =  'Your Auth Key';
+$order = $objectManager->get('Magento\Sales\Model\Order')->loadByIncrementId('100000094');
+
+$helper->createOrderShipment($auth_key, $order, '2016-06-17 17:00', "Merchant/Sender's Name", "Merchant/Sender's Phone", 'Pickup Address Pin', 'Pickup Address');
+```
